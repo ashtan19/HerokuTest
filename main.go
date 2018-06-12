922f5530524d455d4fb1f8803c063c92bd55bb14
Ash Tan <ashtan19@gmail.com>
2018-06-11 17:49:56 -0700

    Test Helloworld onto Heroku

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"encoding/json"
 	"os"
 )
-
+go 
 func determineListenAddress() (string, error) {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -17,8 +17,23 @@ func determineListenAddress() (string, error) {
 	return ":" + port, nil
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+  }
+
 
 func main() {
+	addr, err := determineListenAddress()
+	if err != nil {
+	  log.Fatal(err)
+	}
+	http.HandleFunc("/", hello)
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+	  panic(err)
+	}
+
+
 	response, err := http.Get("https://api.coinbase.com/v2/prices/spot?currency=USD")
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
@@ -26,6 +41,7 @@ func main() {
 		data, _ := ioutil.ReadAll(response.Body)
 		fmt.Printf(string(data))
 	}
+	 /*
 	jsonData := map[string]string{"firstname":"Maple", "lastname": "Liu"}
 	jsonValue, _ := json.Marshal(jsonData)
 	//Requesting with a Header, Can add more customization
@@ -40,5 +56,5 @@ func main() {
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
 		fmt.Printf(string(data))
-	}
+	} */
 }
